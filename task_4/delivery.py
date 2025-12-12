b6769dece00b5bf4123c315aa52038aeef0e5a4b
faloger <gtllop098@gmail.com>
2025-12-12 21:51:57 +0300

    Update delivery.py

--- task_4/delivery.py
+++ task_4/delivery.py
@@ -1,1 +1,12 @@
+import pandas as pd
+
+class DeliveryCalculator:
+    def __init__(self, cost_per_km: float):
+        self.cost_per_km = cost_per_km
+
+    def calculate(self, df: pd.DataFrame) -> pd.DataFrame:
+        df["Базовая стоимость"] = df["Расстояние (км)"] * self.cost_per_km
+        df["Наценка за вес"] = df["Вес груза (кг)"].apply(lambda w: 500 if w > 50 else 0)
+        df["Итого к оплате"] = df["Базовая стоимость"] + df["Наценка за вес"]
+        return df
 
